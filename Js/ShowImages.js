10c8ae3047be67f7af885ce0a519e6da7c3bdaa3
ma-karov <99077921+ma-karov@users.noreply.github.com>
2024-01-30 22:40:39 +0300

    Update ShowImages.js

--- Js/ShowImages.js
+++ Js/ShowImages.js
@@ -5,14 +5,22 @@ var DivPrintImagesHTML = "";
 for (var i = 2; i < IMAGES_OVEN_LENGTH; i += 2) 
     DivPrintImagesHTML += ` 
     <div> 
-        <img src = "Files/Images/Image${i - 1}.jpg" alt = "Image" class = "Class_OurWorks_Main_DivPrintImages_Images" /> 
-        <img src = "Files/Images/Image${i}.jpg" alt = "Image" class = "Class_OurWorks_Main_DivPrintImages_Images" /> 
+        <img data = "Files/Images/Image${i - 1}.jpg" alt = "Image" class = "Class_OurWorks_Main_DivPrintImages_Images" /> 
+        <img data = "Files/Images/Image${i}.jpg" alt = "Image" class = "Class_OurWorks_Main_DivPrintImages_Images" /> 
     </div> `; 
-
 ID_OurWorks_Main_DivPrintImages.innerHTML = DivPrintImagesHTML; 
 
 
+const CONST_INTERSECTION_OBSERVER = new IntersectionObserver(function(ArrayObjects, Observer) 
+{ 
+    ArrayObjects.forEach(Object => 
+        { 
+            Object[`target`][`src`] = Object.intersectionRatio ? Object[`target`].getAttribute(`data`) : ``; 
+        }); 
+}, { root: null, rootMargin: `0px`, threshold: 0.1 }); 
 
+const ARRAY_CLASS_OUR_WORKS_MAIN_DIV_PRINT_IMAGES = document.getElementsByClassName(`Class_OurWorks_Main_DivPrintImages_Images`); 
 
-
+for (const CONST_IMAGE of ARRAY_CLASS_OUR_WORKS_MAIN_DIV_PRINT_IMAGES) 
+    CONST_INTERSECTION_OBSERVER.observe(CONST_IMAGE); 
 
