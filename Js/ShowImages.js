ff142248e201d926f271d3c5623a95f2f8f8773c
ma-karov <99077921+ma-karov@users.noreply.github.com>
2024-01-31 12:27:15 +0300

    Update ShowImages.js

--- Js/ShowImages.js
+++ Js/ShowImages.js
@@ -1,26 +1,29 @@
-
-const IMAGES_OVEN_LENGTH = 25; 
-
-var DivPrintImagesHTML = ""; 
-for (var i = 2; i < IMAGES_OVEN_LENGTH; i += 2) 
-    DivPrintImagesHTML += ` 
-    <div> 
-        <img data = "Files/Images/Image${i - 1}.jpg" alt = "Image" class = "Class_OurWorks_Main_DivPrintImages_Images" /> 
-        <img data = "Files/Images/Image${i}.jpg" alt = "Image" class = "Class_OurWorks_Main_DivPrintImages_Images" /> 
-    </div> `; 
-ID_OurWorks_Main_DivPrintImages.innerHTML = DivPrintImagesHTML; 
-
-
-const CONST_INTERSECTION_OBSERVER = new IntersectionObserver(function(ArrayObjects, Observer) 
-{ 
-    ArrayObjects.forEach(Object => 
-        { 
-            Object[`target`][`src`] = Object.intersectionRatio ? Object[`target`].getAttribute(`data`) : ``; 
-        }); 
-}, { root: null, rootMargin: `0px`, threshold: 0.1 }); 
-
 const ARRAY_CLASS_OUR_WORKS_MAIN_DIV_PRINT_IMAGES = document.getElementsByClassName(`Class_OurWorks_Main_DivPrintImages_Images`); 
 
-for (const CONST_IMAGE of ARRAY_CLASS_OUR_WORKS_MAIN_DIV_PRINT_IMAGES) 
-    CONST_INTERSECTION_OBSERVER.observe(CONST_IMAGE); 
+window.addEventListener(`scroll`, function() 
+{ 
+    function GetOffsetForScrollObjectHTML(ObjectHTML) 
+    { 
+        var ObjectHTML_BoundingClientRect = ObjectHTML.getBoundingClientRect(), 
+        ObjectHTML_BoundingClientRectScrolls = new Array( window[`pageXOffset`] || document[`documentElement`][`scrollLeft`], window[`pageYOffset`] || document[`documentElement`][`scrollTop`] ); 
+        return new Array( ObjectHTML_BoundingClientRect[`left`] + ObjectHTML_BoundingClientRectScrolls[0], 
+                            ObjectHTML_BoundingClientRect[`top`] + ObjectHTML_BoundingClientRectScrolls[1] ); 
+    }
+
+    for (const CONST_IMAGE of ARRAY_CLASS_OUR_WORKS_MAIN_DIV_PRINT_IMAGES) 
+        if (!CONST_IMAGE[`src`])
+        { 
+            const CONST_IMAGE_HEIGHT = CONST_IMAGE[`offsetHeight`], CONST_IMAGE_HEIGHT_OFFSET = GetOffsetForScrollObjectHTML(CONST_IMAGE)[1]; 
+            console.log(CONST_IMAGE_HEIGHT, CONST_IMAGE_HEIGHT_OFFSET); 
+
+            const THRESHOLD = 0.01, WINDOW_INNER_HEIGHT = window[`innerHeight`], WINDOW_PAGE_Y_OFFSET = window[`pageYOffset`]; 
+            var ConstImagePointVisible = WINDOW_INNER_HEIGHT - THRESHOLD*CONST_IMAGE_HEIGHT; 
+            if (ConstImagePointVisible > WINDOW_INNER_HEIGHT) 
+                ConstImagePointVisible = WINDOW_INNER_HEIGHT*(1 - THRESHOLD); 
+
+            if (WINDOW_PAGE_Y_OFFSET > CONST_IMAGE_HEIGHT_OFFSET - ConstImagePointVisible && WINDOW_PAGE_Y_OFFSET < CONST_IMAGE_HEIGHT_OFFSET + ConstImagePointVisible)
+                CONST_IMAGE[`src`] = CONST_IMAGE.getAttribute(`data`); 
+        } 
+    
+})
 
